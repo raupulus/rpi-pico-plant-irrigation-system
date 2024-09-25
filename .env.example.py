b16861a2541abcd7ba25226332e1233bd79e7a8b
Raúl Caro Pastorino <raul@fryntiz.dev>
2024-09-26 01:35:26 +0200

    Created .env example file with essential vars

---
+++ .env.example.py
@@ -0,0 +1,17 @@
+# Wireless
+AP_NAME = ""
+AP_PASS = ""
+
+# Datos para la API
+API_URL = "http://localhost:8000/api/v1"
+API_PATH = "ink/resume"
+API_TOKEN = "apitoken"
+
+# Nombre del equipo para identificarlo en la api, id y nombre.
+DEVICE_ID = 1
+
+# Configuración de la pantalla.
+TIME_TO_REFRESH = 10 # Minutos para apagar la pantalla automáticamente
+
+# Indica si está en modo debug la aplicación
+DEBUG = False
